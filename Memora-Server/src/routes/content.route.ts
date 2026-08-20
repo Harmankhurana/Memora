c8130d88cd61f8e9b79c63a24482cdcff4966381
Harman khurana <work.harmankhurana@gmail.com>
2026-08-20 09:09:03 +0530

    feat(routes): scaffold content API routes for create, fetch, and delete operations

---
+++ Memora-Server/src/routes/content.route.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+const contentRouter = Router();
+
+contentRouter.post('/content', )
+contentRouter.get('/content', );
+contentRouter.delete('/content', );
+
+export default contentRouter
