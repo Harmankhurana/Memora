1f0cef5d9f0254addc0d4af5e40ecfcdcf9da2ce
Harman khurana <work.harmankhurana@gmail.com>
2026-08-21 02:14:43 +0530

    feat: add content route handlers for create, fetch, and delete operations

--- Memora-Server/src/routes/auth.route.ts
+++ Memora-Server/src/routes/auth.route.ts
@@ -4,6 +4,6 @@ import { signup, signin } from "../controllers/auth.controller.js";
 const authRouter = Router();
 
 authRouter.post('/signup', signup);
-authRouter.post('signin', signin);
+authRouter.post('/signin', signin);
 
 export default authRouter;
--- Memora-Server/src/routes/content.route.ts
+++ Memora-Server/src/routes/content.route.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
+import { postContent, getContent, deleteContent } from "../controllers/content.controller.js";
 
 const contentRouter = Router();
 
-contentRouter.post('/content', )
-contentRouter.get('/content', );
-contentRouter.delete('/content', );
+contentRouter.post('/', postContent)
+contentRouter.get('/', getContent);
+contentRouter.delete('/', deleteContent);
 
 export default contentRouter
