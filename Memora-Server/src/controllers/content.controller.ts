0ab104e8d7989a3b82d150e14fc7517704c3b292
Harman khurana <work.harmankhurana@gmail.com>
2026-08-21 01:56:38 +0530

    feat(content): implement get route in content API endpoint

--- Memora-Server/src/controllers/content.controller.ts
+++ Memora-Server/src/controllers/content.controller.ts
@@ -1,5 +1,6 @@
 import type { Request, Response } from "express";
 import { ContentModel } from "../models/content.model.js";
+import { connect } from "node:http2";
 
 export const postContent = async (req: Request, res: Response) => {
     const { link, type, title, tags } = req.body;
@@ -22,16 +23,32 @@ export const postContent = async (req: Request, res: Response) => {
 
         res.status(200).json({
             message: "Content added successfully",
-        })
+        });
+
     } catch (e) {
         res.status(500).json({
             message: "Something went wrong while adding content",
+            error: e,
         });
     }
 }
 
-export const getContent = async (req: Request, res: Response): Promise<void> => {
-    
+export const getContent = async (req: Request, res: Response) => {
+    // @ts-ignore
+    const userId = req.userId;
+
+    try {
+        const contents = await ContentModel.find({ userId }).populate("userId", "username");
+
+        return res.status(200).json({
+            message: "Here is you content",
+            contents,
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: "Error occured in fetching content"
+        });
+    }
 } 
 
 export const deleteContent = async (req: Request, res: Response): Promise<void> => {
