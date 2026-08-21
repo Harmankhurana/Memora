fd25b43dc40e8f7e88473ca7d0806671c07805a0
Harman khurana <work.harmankhurana@gmail.com>
2026-08-21 09:36:59 +0530

    feat(share): implement share link creation and retrieval with user content lookup and random hash generation

--- Memora-Server/src/controllers/share.controller.ts
+++ Memora-Server/src/controllers/share.controller.ts
@@ -1,7 +1,48 @@
 import type { Request, Response } from "express";
+import { LinkModel } from "../models/content.model.js";
+import { randomLink } from "../utils/randomLink.utils.js";
 
 export const postShareLink = async(req: Request, res: Response) => {
+    const share = req.body.share;
 
+    if(share) {
+        const existingLink = await LinkModel.findOne({
+            // @ts-ignore
+            userId: req.userId,
+        });
+
+        if(existingLink) {
+            res.json({
+                hash: existingLink.hash,
+            });
+            return;
+        }
+
+        const hash = randomLink(10);
+        await LinkModel.create({
+            // @ts-ignore
+            userId: req.userId,
+            hash: hash
+        });
+
+        res.json({
+            message: "/share/" + hash,
+        });
+
+    } else {
+        await LinkModel.deleteOne({
+            //@ts-ignore
+            userId: req.userId,
+        });
+
+        res.json({
+            message: "Link Removed",
+        });
+    }
+
+    res.status(200).json({
+        message: "Updated shareable link stored in database",
+    });
 }
 
 export const getShareLink = async(req: Request, res: Response) => {
