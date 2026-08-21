6400283e54c8fff669f0def16f5ee47193a37b0b
Harman khurana <work.harmankhurana@gmail.com>
2026-08-21 09:22:12 +0530

    feat: add share link controller skeleton with post and get handlers

---
+++ Memora-Server/src/controllers/share.controller.ts
@@ -0,0 +1,9 @@
+import type { Request, Response } from "express";
+
+export const postShareLink = async(req: Request, res: Response) => {
+
+}
+
+export const getShareLink = async(req: Request, res: Response) => {
+
+}
